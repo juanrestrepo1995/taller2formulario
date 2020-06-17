7e6c621a35d5054cd1d230977c8ce34215fc4cdb
juanrestrepo1995 <62155773+juanrestrepo1995@users.noreply.github.com>
2020-06-17 02:33:19 -0500

    taller 2 formulario

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>formulario</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<body>
+<header>
+    
+
+</header>
+<main>
+<div class="container">
+    <div class="row Justify-content-start">
+        <div class="col-12 col-md-5">
+            <h1 class="Justify-content-start">registrate en nuestra web</h1>
+            <h4>crear registro</h4>
+        <form>
+  <div class="form-group">
+    <label for="emailusuario">correo eletronico</label>
+    <input type="email" class="form-control" id="emailusuario" aria-describedby="emailHelp">
+    <small id="emailusuario" class="form-text text-muted">Nunca compartiremos tu correo electrónico con nadie más</small>
+  </div>
+  <div class="form-group">
+    <label for="Passwordususario">contraseña</label>
+    <input type="password" class="form-control" id="Passwordususario">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input Justify-content-start" id="aceptarterminos">
+    <label class="form-check-label" for="exampleCheck1">acepto terminos y condiciones </label>
+  </div>
+  <button type="submit" class="btn btn-primary btn-block">agregar</button>
+</form>
+<br>
+<br>
+<br>
+<h4>consultar registro</h4>
+<form>
+<input class="form-control form-control-lg" type="text" placeholder="nombre usuario">
+<br>
+<button type="submit" class="btn btn-success btn-block">buscar</button>
+<br>
+<br>
+<br>
+<br>
+<h4>eliminar registro</h4>
+<br>
+<br>
+<h6>eliminar usuario</h6>
+<form>
+<input class="form-control form-control-lg" type="text" placeholder="nombre usuario">
+<br>
+<button type="submit" class="“btn btn-danger btn-block">actualizar</button>
+
+        </div>
+    <div class="row Justify-content-start">
+            <div class="col-12 col-md-7">
+                <h2>resultados</h2>
+
+            </div>
+
+        </div>
+     </div>
+    
+        
+        
+    </div>
+    
+
+</div>
+</main>
+<footer>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</footer>
+</body>
+</html>
